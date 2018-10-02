b0bcb636270f47de0569b000271a5c0e5ca845a6
Evgeniy <skopin.evgenii@gmail.com>
2018-10-02 22:30:05 +0300

    fix & reveal proj

---
+++ reveal/JQUERY Main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("#box1 .colorLayer").animate({left:"0px"},300);
+    $("#box1 .colorLayer").delay(400).animate({left:"650px"},300);
+    $("#box1 .title").animate({left:"0px"});
+    
+    $("#box2 .colorLayer").delay(300).animate({left:"0px"},300);
+    $("#box2 .colorLayer").delay(400).animate({left:"650px"},300);
+    $("#box2 .title").delay(300).animate({left:"0px"});
+    
+});
+
+
+
+
+
+
+
+
