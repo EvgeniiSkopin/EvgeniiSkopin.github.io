447ff920c07c5b5e603ee3449b2cef317a9b0149
EvgeniiSkopin <30588928+EvgeniiSkopin@users.noreply.github.com>
2018-10-12 13:10:52 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-initOnMouse();
-
-function initOnMouse(){
-    document.onmousemove=mousemove;
-}
-
-function mousemove(){
-    let mouse_x = y = 0;
-    if (document.attachEvent != null) {
-        mouse_x = window.event.clientX;
-        mouse_y = window.event.clientY;
-    } else if (!document.attachEvent && document.addEventListener) {
-        mouse_x = event.clientX;
-        mouse_y = event.clientY;
-    }
-    document.getElementById('cursorX').innerHTML = "x: " + mouse_x;
-    document.getElementById('cursorY').innerHTML = "y: " + mouse_y;
-
-    document.getElementsByClassName('vert-line')[0].style.left = mouse_x + 20;
-    document.getElementsByClassName('hor-line')[0].style.top = mouse_y - 20;
-}
+$(document).ready(function(){
+    $("#box1 .colorLayer").animate({left:"0px"},300);
+    $("#box1 .colorLayer").delay(400).animate({left:"500px"},300);
+    $("#box1 .backGroundLayer").delay(800).animate({left:"0px"},500);
+    $("#box1 .title").animate({left:"0px"});
+    
+    $("#box2 .colorLayer").delay(300).animate({left:"0px"},300);
+    $("#box2 .colorLayer").delay(400).animate({left:"500px"},300);
+    $("#box2 .backGroundLayer").delay(1100).animate({left:"0px"},500);
+    $("#box2 .title").delay(300).animate({left:"0px"});
+    
+});
 
